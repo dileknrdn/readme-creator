99090571977863a3e72a233fee816161ec1680dc
Dilek Nuredin <dileknuredin@hotmail.com>
2024-02-07 15:29:31 -0500

    added the basic structure to index.js

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -13,8 +13,46 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ![License](https://img.shields.io/badge/License-${data.license}-blue.svg)
+  ## Description
+  ${data.description}
 
-`;
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contribution](#contribution)
+  - [Tests](#tests)
+  - [License](#license)
+  - [Questions](#questions)
+  
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+
+  ${data.usage}
+
+  ## Contribution
+
+  ${data.contribution}
+
+  ## Tests
+
+  ${data.test}
+
+  ## License
+
+  ${data.license}
+
+  ## Questions
+
+  If you have any questions, please reach out to me at ${data.email} or visit my GitHub profile at [${data.github}](
+
+  )`;
 }
 
 module.exports = generateMarkdown;
+
+// Path: readme-generator/index.js
+// Compare this snippet from readme-generator/generateMarkdown.js:
+// const generateMarkdown = require("./generateMarkdown.js");
