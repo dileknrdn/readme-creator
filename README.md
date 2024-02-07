83122bde2c340a60675daffd66451f0cb2d90c64
Dilek Nuredin <dileknuredin@hotmail.com>
2024-02-07 15:48:49 -0500

    updated the index.js meet the requirements

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# Readme Generator
+# 
+  ![License](https://img.shields.io/badge/License-MIT-blue.svg)
+  ## Description
+  
+
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contribution](#contribution)
+  - [Tests](#tests)
+  - [License](#license)
+  - [Questions](#questions)
+  
+  ## Installation
+  
+
+  ## Usage
+
+  
+
+  ## Contribution
+
+  
+
+  ## Tests
+
+  
+
+  ## License
+
+  MIT
+
+  ## Questions
+
+  If you have any questions, please reach out to me at  or visit my GitHub profile at [](
+
+  )
--- index.js
+++ index.js
@@ -2,71 +2,69 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const generateMarkdown = require("./generateMarkdown.js");
+const path = require("path");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "What is the title of your project?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Please provide a description of your project.",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "Please provide installation instructions for your project.",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Please provide usage information for your project.",
+  },
+  {
+    type: "input",
+    name: "contribution",
+    message: "Please provide contribution guidelines for your project.",
+  },
+  {
+    type: "input",
+    name: "test",
+    message: "Please provide test instructions for your project.",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "Please choose a license for your project.",
+    choices: ["MIT", "GNU GPLv3", "Apache 2.0", "ISC", "None"],
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "Please provide your GitHub username.",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Please provide your email address.",
+  },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-  fs.writeFile(fileName, data, (err) =>
-    err ? console.error(err) : console.log("Success!")
-  );
+  return fs.writeFileSync(path.join(process.cwd(), fileName), data);
 }
 
 // TODO: Create a function to initialize app
 function init() {
-  inquirer
-    .prompt([
-      {
-        type: "input",
-        name: "title",
-        message: "What is the title of your project?",
-      },
-      {
-        type: "input",
-        name: "description",
-        message: "Please provide a description of your project.",
-      },
-      {
-        type: "input",
-        name: "installation",
-        message: "Please provide installation instructions for your project.",
-      },
-      {
-        type: "input",
-        name: "usage",
-        message: "Please provide usage information for your project.",
-      },
-      {
-        type: "input",
-        name: "contribution",
-        message: "Please provide contribution guidelines for your project.",
-      },
-      {
-        type: "input",
-        name: "test",
-        message: "Please provide test instructions for your project.",
-      },
-      {
-        type: "list",
-        name: "license",
-        message: "Please choose a license for your project.",
-        choices: ["MIT", "GNU GPLv3", "Apache 2.0", "ISC", "None"],
-      },
-      {
-        type: "input",
-        name: "github",
-        message: "Please provide your GitHub username.",
-      },
-      {
-        type: "input",
-        name: "email",
-        message: "Please provide your email address.",
-      },
-    ])
-    .then((response) => {
-      writeToFile("README.md", generateMarkdown(response));
-    });
+  inquirer.prompt(questions).then((inquirerResponses) => {
+    console.log("Generating README...");
+    writeToFile("README.md", generateMarkdown({ ...inquirerResponses }));
+  });
 }
 
 // Function call to initialize app
